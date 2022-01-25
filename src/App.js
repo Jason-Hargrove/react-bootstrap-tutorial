788c087302649df4e13b8b1267ba0f81e0b1919d
Jason Hargrove <rijaha@gmail.com>
2022-01-24 19:38:50 -0600

    My first commit.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button, Card } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card>
+          <Card.Body>
+            <Card.Img style={{ width: "400px"}} variant="top" src="/img/the-truth.jpg"/>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>Card text...</Card.Text>
+
+            <Button variant="primary">This is a button</Button>
+          </Card.Body>
+        </Card>
+        <Button variant="secondary">This is a button</Button>
       </header>
     </div>
   );
