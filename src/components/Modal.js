bbf7e2f298b24efb35645e065c852b6595ebccbd
Jason Hargrove <rijaha@gmail.com>
2022-01-28 22:49:27 -0600

    Added an alert.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,26 @@
-import { Button } from "react-bootstrap";
+import { Button, Alert } from "react-bootstrap";
 const title = "I'm a Model";
 
 export function MyModal() {
-    return (
-        <>
-            <Button>
-                {title}
-            </Button>
-        </>
-    )
+  const alerts = [
+    "primary",
+    "secondary",
+    "success",
+    "danger",
+    "warning",
+    "info",
+    "light",
+    "dark",
+  ].map((variant, idx) => (
+    <Alert key={idx} variant={variant}>
+      This is a {variant} alert—check it out!
+    </Alert>
+  ));
 
+  return (
+    <>
+      <Button>{title}</Button>
+      {alerts}
+    </>
+  );
 }
