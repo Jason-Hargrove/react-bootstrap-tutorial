5a0a3fb58947091a9ac9cce8e446f9bb888a2872
Jason Hargrove <rijaha@gmail.com>
2022-02-02 21:16:06 -0600

    Added a button.

--- src/components/Layout2.js
+++ src/components/Layout2.js
@@ -1,9 +1,13 @@
+import { Button } from 'react-bootstrap';
+
 const title = "Groovy - Layout 2"
+
 
 export function Layout2() {
     return (
         <>
             <h1>{title}</h1>
+            <Button variant="primary">Something</Button>
         </>
-    )
+    );
 }
